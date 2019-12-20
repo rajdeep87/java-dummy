40b2161b6ce8a4f59a4a2b41c450b7290d6e35e4
Mukherjee <mukherr@a483e7ab763c.ant.amazon.com>
2019-12-20 13:16:22 -0800

    nudge

--- pkg/QuickSort.java
+++ pkg/QuickSort.java
@@ -50,8 +50,8 @@ class QuickSort
 		{
 			/* pi is partitioning index, arr[pi] is
 			now at right place */
-			int pi = partition(arr, low, high);
-
+			int pi = partition(arr, low+1, high-1);
+                        if(pi > 0) sort(arr, 0 , 1);
 			// Recursively sort elements before
 			// partition and after partition
 			sort(arr, low, pi-1);
